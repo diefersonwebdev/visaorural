20cf6791da15aea80210fcff26225b253f8fc67d
diefersonwebdev <diefersonsoares@hotmail.com>
2015-05-13 00:18:05 -0300

    Projeto Visão Rural

--- index.php
+++ index.php
@@ -6,6 +6,26 @@
         <link href="style.css" rel="stylesheet">
     </head>
     <body>
+        <section id="container">
+            <section id="containerHeader">
+                <section class="headerlogotipo"><a href="index.php"><img src="img/logotipovisaorural.png" alt="Visão Rural" width="250" height="200"></a>
+                </section><!--HEADER LOGOTIPO-->
+            </section><!--SECTION RESPONSÁVEL PELO HEADER DO SITE-->
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        </section><!--SECTION RESPONSÁVEL POR TODA A ESTRUTURA DO SITE-->
         
     </body>
 </html>
